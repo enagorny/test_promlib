09c0e58a8ab45dfe4ce2ee648d74ae7ab811616a
enagorny <ideviantik@gmail.com>
2013-09-09 20:33:00 +0000

    Login which not login added; on tutorial track

--- promlib/views.py
+++ promlib/views.py
@@ -1,8 +1,29 @@
 
-from flask import render_template
-
+from flask import (
+    render_template,
+    flash,
+    redirect,
+)
 
 from promlib import app
+from promlib.forms import LoginForm
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data +
+              '", remember_me=' + str(form.remember_me.data))
+        return redirect('/index')
+
+    context = {
+        'title': 'Sign In',
+        'form': form,
+        'providers': app.config['OPENID_PROVIDERS'],
+    }
+    return render_template('login.html', **context)
+
 
 @app.route('/')
 @app.route('/index')
